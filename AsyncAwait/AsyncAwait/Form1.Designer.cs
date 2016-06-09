da1102ef0f0f2b189f9ce15579990fbd9fe84be5
Vishal Kumar <v.kumar@tavant.com>
2016-06-09 14:46:53 +0530

    Async await project added

---
+++ AsyncAwait/AsyncAwait/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace AsyncAwait
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDone = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(201, 136);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(75, 23);
+            this.btnDone.TabIndex = 0;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(129, 56);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(244, 20);
+            this.txtResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 289);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnDone);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ AsyncAwait/AsyncAwait/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwait
+{
+    /* 
+        Summarize the key points of this example, as follows:
+        ------------------------------------------------------
+
+        • Methods (as well as lambda expressions or anonymous methods) can be marked
+        with the async keyword to enable the method to do work in a nonblocking
+        manner.
+
+        • Methods (as well as lambda expressions or anonymous methods) marked with the
+        async keyword will run in a blocking manner until the await keyword is
+        encountered.
+
+        • A single async method can have multiple await contexts.
+
+        • When the await expression is encountered, the calling thread is suspended until
+        the awaited task is complete. In the meantime, control is returned to the caller of
+        the method.
+
+        • The await keyword will hide the returned Task object from view, appearing to
+        directly return the underlying return value. Methods with no return value simply
+        return void.
+
+        • As a naming convention, methods that are to be called asynchronously should be
+        marked with the “Async” suffix. 
+    
+    */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnDone_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = await DoneWork();
+        }
+
+        private async Task<string> DoneWork()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                return "Done with work!";
+            });
+        }
+    }
+}
